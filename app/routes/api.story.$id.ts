51cc4b991378909bef1cb3e0761b95b010115189
Vo Huu Nam Truong <namtruong0900@gmail.com>
2025-06-05 14:23:51 +0700

    + fix url

--- app/routes/api.story.$id.ts
+++ app/routes/api.story.$id.ts
@@ -1,11 +1,13 @@
 import { json } from "@remix-run/node";
 
+const API_BASE_URL = process.env.AUTO_VN_GEN_API_URL || "http://localhost:8000";
+
 export const action = async ({ params, request }: any) => {
   if (request.method !== "DELETE") {
     return json({ error: "Method not allowed" }, { status: 405 });
   }
   const { id } = params;
-  const apiUrl = `http://localhost:8000/story/${id}`;
+  const apiUrl = `${API_BASE_URL}/story/${id}`;
 
   const res = await fetch(apiUrl, { method: "DELETE" });
   if (!res.ok) {
